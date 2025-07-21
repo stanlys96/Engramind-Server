b6a71c69edf163e42e7f4d73addaffcef2802bff
stanlys96 <stanlyskwok@gmail.com>
2025-07-22 06:23:38 +0700

    update .env

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // allow your frontend origin
-    credentials: true, // if using cookies or Authorization headers
+    origin: 'https://oi22n-uaaaa-aaaaf-qawqq-cai.icp0.io/',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
 }
