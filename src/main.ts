f15a5784c80f60e3bb7735e6a60185c9bd16868f
stanlys96 <stanlyskwok@gmail.com>
2025-07-22 06:25:11 +0700

    update .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://oi22n-uaaaa-aaaaf-qawqq-cai.icp0.io/',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
