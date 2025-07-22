4d8223ff6d192942ebbff5c2f0ceaad58a58fb3f
stanlys96 <stanlyskwok@gmail.com>
2025-07-22 15:05:31 +0700

    update for pg

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
@@ -20,17 +14,9 @@ export class User {
   @Column()
   password: string;
 
-  @CreateDateColumn({
-    type: 'timestamptz',
-    nullable: true,
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  createdAt?: Date;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
 
-  @UpdateDateColumn({
-    type: 'timestamptz',
-    nullable: true,
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  updatedAt?: Date;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
 }
