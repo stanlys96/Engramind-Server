5218cf19281dbeab3809b528b448b79073d33ad6
stanlys96 <stanlyskwok@gmail.com>
2025-08-02 15:21:18 +0700

    add 1 more origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
+    origin: [
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL_2,
+      process.env.FRONTEND_URL_3,
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
